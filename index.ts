d081abaf2cc3f5d81035577c13e8a34137d76732
Doom Sébastien <88394869+Zansuken@users.noreply.github.com>
2024-09-27 13:16:39 +0200

    Refactor project structure and dependencies

--- index.ts
+++
@@ -1,8 +0,0 @@
-import Button from "./src/components/Button/Button";
-import DatePicker from "./src/components/DatePicker/DatePicker";
-import Dialog from "./src/components/Dialog/Dialog";
-import Input from "./src/components/Input/Input";
-import Select from "./src/components/Select/Select";
-import Table from "./src/components/Table/Table";
-
-export { Button, DatePicker, Dialog, Input, Select, Table };
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import "./index.css";
+import Button from "./components/Button/Button";
+import DatePicker from "./components/DatePicker/DatePicker";
+import Dialog from "./components/Dialog/Dialog";
+import Input from "./components/Input/Input";
+import Select from "./components/Select/Select";
+import Table from "./components/Table/Table";
+
+export { Button, DatePicker, Dialog, Input, Select, Table };
