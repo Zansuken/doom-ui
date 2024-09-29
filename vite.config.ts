17c6802301e58ef0a73c730ec27a5612953f1a9e
Doom Sébastien <88394869+Zansuken@users.noreply.github.com>
2024-09-29 15:58:42 +0200

    Update Vite configuration and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libInjectCss(), dts({ include: ["src"] })],
   server: {
     open: true,
   },
